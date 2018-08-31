2514bf168cd829a5f44477ae8cc0b9ca04bf6934
Van Even Stijn <vanevenstijn@hotmail.com>
2018-08-31 16:49:38 +0200

    Server Connection
    Tested Server Connection ==> Spaghetti code (Will clean up and make this generic)
    
    Read all appnames and paste them in a list.

--- QSMGMT/QSMGMT/Form1.Designer.cs
+++ QSMGMT/QSMGMT/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.Version = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Version
+            // 
+            this.Version.AutoSize = true;
+            this.Version.Location = new System.Drawing.Point(113, 58);
+            this.Version.Name = "Version";
+            this.Version.Size = new System.Drawing.Size(64, 25);
+            this.Version.TabIndex = 0;
+            this.Version.Text = "label1";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 24;
+            this.listBox1.Location = new System.Drawing.Point(945, 139);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(463, 364);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1806, 907);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Version);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Version;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- QSMGMT/QSMGMT/Form1.cs
+++ QSMGMT/QSMGMT/Form1.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Security.Cryptography.X509Certificates;
+using Qlik.Engine;
+using Qlik.Engine.Communication;
+using System.IO;
 
 namespace QSMGMT
 {
@@ -15,6 +19,51 @@ namespace QSMGMT
         public Form1()
         {
             InitializeComponent();
+
+        }
+        internal static byte[] ReadFile(string fileName)
+        {
+            FileStream f = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            int size = (int)f.Length;
+            byte[] data = new byte[size];
+            size = f.Read(data, 0, size);
+            f.Close();
+            return data;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            ILocation location = null;
+            try
+            {
+                var uri = new Uri("https://qliksense.credon.eu:4747");
+                location = Qlik.Engine.Location.FromUri(uri);
+                X509Certificate2 x509 = new X509Certificate2(@"C:\\GITHUB\\QSMGMT\\QSMGMT\\QSMGMT\\QS Certificates\\CRED DEMO SERVER\\CREDON35\\client.pfx","test");
+                location.IsVersionCheckActive = false;
+                X509Certificate2Collection certificateCollection = new X509Certificate2Collection(x509);
+                // Defining the location as a direct connection to Qlik Sense Server
+                location.AsDirectConnectionAsync("DEMO", "QSADMIN", certificateValidation: false, certificateCollection: certificateCollection); 
+            }
+            catch (CommunicationErrorException cex)
+            {
+                Console.WriteLine("Can not connect to Qlik Sense instance, check that Qlik Sense is running." + Environment.NewLine + cex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("General error." + Environment.NewLine + ex.Message);
+            }
+
+            //  String[] test = new String[] { "test"}; 
+
+            List<String> test = new List<String>();
+
+            foreach (IAppIdentifier appIdentifier in location.GetAppIdentifiers())
+            {
+                Console.WriteLine(appIdentifier.AppName);
+                test.Add(appIdentifier.AppName);
+            }
+
+            listBox1.DataSource = test;
         }
     }
 }
